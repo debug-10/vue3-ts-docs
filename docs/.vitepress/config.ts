e72dfa97cffc1ec30917516dadd4c999a658f820
debug-10 <37249116@qq.com>
2024-10-08 09:54:10 +0800

    前端工程化学习文档仓库初始化

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+
+export default defineConfig({
+    title: "Vue 3 + Typescript 学习文档",
+    description: "详细学习 Vue 3 和 TypeScript 的指南",
+    themeConfig: { 
+        // siteTitle: "前端学习” ,
+        logo: "/assets/V.png",
+        nav: [
+            { text:"首页", link: "/" },
+            { text:"指南", link:"/guide/"},
+            { text:"组件", link:"/components/"},
+            { text:"API 参考", link:"/api/"},
+            { text:"常见问题", link:"/fag/"},
+            ],
+        socialLinks: [
+            { icon: "github", link:"https://github.com/vuejs/vitepress" },
+            ],
+        sidebar: {
+         "/guide/": [
+         {
+           text: "开始",
+           collapsible: true,
+           items: [
+           { text:"介绍" , link:"/quide/"},
+           { text:"安装" , link:"/guide/installation" }, 
+           { text:"基本概念" , link:"/guide/concepts" },
+          ],
+         },
+        ],
+         "components/": [
+         {
+           text: "常用组件",
+           items: [
+           { text:"介绍" , link:"/components/"},
+           { text:"按钮 Button" , link:"/components/button" }, 
+           { text:"表单 Form" , link:"/components/form" },
+           { text:"介绍 Table" , link:"/components/table"},
+          ],
+         },
+        ],
+       },
+    footer: {
+        message: "用心学习",
+        copyright: "2024©guke"
+    },  
+    },
+});
---
+++ docs/index.md
@@ -0,0 +1,42 @@
+---
+# Hello World
+
+layout: home
+
+hero: 
+  name: "前端工程化"
+  text: "Vue 3 + TypeScript 学习文档"
+  tagline: "学习是少数人的狂欢，亦是孤独者的游戏"
+  image: 
+    src: /assets/V.png
+    alt: Chrome 浏览器插件
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /guide/
+    - theme: alt
+      text: 常见问题
+      link: /faq/
+      
+      
+features:  
+  - icon: 
+    title: 轻松入门
+    details: 从头开始，循序渐进
+  - icon: 
+    title: 最佳实践
+    details: 实践       
+  - icon: 
+    title: 高效开发
+    details: 高效
+
+
+---
+
+<div style = "text-align: center; margin-top: 50px;">
+ <em> Happy Coding! </em>
+</div> 
+
+
+
+
